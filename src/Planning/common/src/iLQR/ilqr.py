8aaeb6b33ddd4071f469c4c0c69f4e54062a99de
Truck NX7 <dy8451@princeton.edu>
2022-04-30 02:20:29 +0800

    core_acc - converted tabs to spaces

--- src/Planning/common/src/iLQR/ilqr.py
+++ src/Planning/common/src/iLQR/ilqr.py
@@ -6,123 +6,123 @@ import time
 
 class iLQR():
 
-	def __init__(self, cost: Cost, params):
-		self.T = params['T']
-		self.N = params['N']
+    def __init__(self, cost: Cost, params):
+        self.T = params['T']
+        self.N = params['N']
 
-		self.steps = params['max_itr']
+        self.steps = params['max_itr']
 
-		self.tol = 1e-2
-		self.lambad = 10
-		self.lambad_max = 100
-		self.lambad_min = 1e-3
+        self.tol = 1e-2
+        self.lambad = 10
+        self.lambad_max = 100
+        self.lambad_min = 1e-3
 
-		self.dynamics = Dynamics(params)
-		self.alphas = 1.1**(-np.arange(10)**2)
+        self.dynamics = Dynamics(params)
+        self.alphas = 1.1**(-np.arange(10)**2)
 
-		self.dim_x = self.dynamics.dim_x
-		self.dim_u = self.dynamics.dim_u
+        self.dim_x = self.dynamics.dim_x
+        self.dim_u = self.dynamics.dim_u
 
-		self.cost = cost
+        self.cost = cost
 
-	def forward_pass(self, nominal_states, nominal_controls, K_closed_loop, k_open_loop, alpha):
-		X = np.zeros_like(nominal_states)
-		U = np.zeros_like(nominal_controls)
+    def forward_pass(self, nominal_states, nominal_controls, K_closed_loop, k_open_loop, alpha):
+        X = np.zeros_like(nominal_states)
+        U = np.zeros_like(nominal_controls)
 
-		X[:, 0] = nominal_states[:, 0]
-		for t in range(self.N-1):
-			K = K_closed_loop[:, :, t]
-			k = k_open_loop[:, t]
-			u = (nominal_controls[:, t] + alpha * k + K @ (X[:, t] - nominal_states[:, t]))
-			X[:, t+1], U[:, t] = self.dynamics.forward_step(X[:, t], u)
-		
-		# calculate the cost of new trajectory
-		J = self.cost.get_cost(X, U)
+        X[:, 0] = nominal_states[:, 0]
+        for t in range(self.N-1):
+            K = K_closed_loop[:, :, t]
+            k = k_open_loop[:, t]
+            u = (nominal_controls[:, t] + alpha * k + K @ (X[:, t] - nominal_states[:, t]))
+            X[:, t+1], U[:, t] = self.dynamics.forward_step(X[:, t], u)
+        
+        # calculate the cost of new trajectory
+        J = self.cost.get_cost(X, U)
 
-		return X, U, J
+        return X, U, J
 
-	def backward_pass(self, nominal_states, nominal_controls):
-		L_x, L_xx, L_u, L_uu, L_ux = self.cost.get_derivatives(nominal_states, nominal_controls)
-		A, B = self.dynamics.get_AB_matrix(nominal_states, nominal_controls)
-		reg_mat = self.lambad * np.eye(self.dim_u)
-		
-		# initialize optimal control
-		k_open_loop = np.zeros((self.dim_u, self.N - 1))
-		K_closed_loop = np.zeros((self.dim_u, self.dim_x, self.N - 1))
-		# set derivatives of value function at final step
-		p = L_x[:, -1]
-		P = L_xx[:, :, -1]
-		for t in range(self.N-2, -1, -1):
-			# compute quality function and its derivatives
-			Q_x = L_x[:, t] + A[:, :, t].T @ p
-			Q_u = L_u[:, t] + B[:, :, t].T @ p
-			Q_xx = L_xx[:, :, t] + A[:, :, t].T @ P @ A[:, :, t]
-			Q_ux = B[:, :, t].T @ P @ A[:, :, t] + L_ux[:, :, t]
-			Q_uu = L_uu[:, :, t] + B[:, :, t].T @ P @ B[:, :, t]
+    def backward_pass(self, nominal_states, nominal_controls):
+        L_x, L_xx, L_u, L_uu, L_ux = self.cost.get_derivatives(nominal_states, nominal_controls)
+        A, B = self.dynamics.get_AB_matrix(nominal_states, nominal_controls)
+        reg_mat = self.lambad * np.eye(self.dim_u)
+        
+        # initialize optimal control
+        k_open_loop = np.zeros((self.dim_u, self.N - 1))
+        K_closed_loop = np.zeros((self.dim_u, self.dim_x, self.N - 1))
+        # set derivatives of value function at final step
+        p = L_x[:, -1]
+        P = L_xx[:, :, -1]
+        for t in range(self.N-2, -1, -1):
+            # compute quality function and its derivatives
+            Q_x = L_x[:, t] + A[:, :, t].T @ p
+            Q_u = L_u[:, t] + B[:, :, t].T @ p
+            Q_xx = L_xx[:, :, t] + A[:, :, t].T @ P @ A[:, :, t]
+            Q_ux = B[:, :, t].T @ P @ A[:, :, t] + L_ux[:, :, t]
+            Q_uu = L_uu[:, :, t] + B[:, :, t].T @ P @ B[:, :, t]
 
-			Q_uu_inv = np.linalg.inv(Q_uu + reg_mat)
-			
-			# update optimal control
-			k_open_loop[:, t] = -Q_uu_inv @ Q_u
-			K_closed_loop[:, :, t] = -Q_uu_inv @ Q_ux
+            Q_uu_inv = np.linalg.inv(Q_uu + reg_mat)
+            
+            # update optimal control
+            k_open_loop[:, t] = -Q_uu_inv @ Q_u
+            K_closed_loop[:, :, t] = -Q_uu_inv @ Q_ux
 
-			# update derivatives of value function
-			p = Q_x - Q_ux.T @ Q_uu_inv @ Q_u
-			P = Q_xx - Q_ux.T @ Q_uu_inv @ Q_ux
+            # update derivatives of value function
+            p = Q_x - Q_ux.T @ Q_uu_inv @ Q_u
+            P = Q_xx - Q_ux.T @ Q_uu_inv @ Q_ux
 
-		return K_closed_loop, k_open_loop
+        return K_closed_loop, k_open_loop
 
-	def solve(self, cur_state, controls=None, obs_list=[], record=False):
-		status = 0
-		self.lambad = 0
+    def solve(self, cur_state, controls=None, obs_list=[], record=False):
+        status = 0
+        self.lambad = 0
 
-		time0 = time.time()
+        time0 = time.time()
 
-		if controls is None:
-			controls = np.zeros((self.dim_u, self.N))
-		states = np.zeros((self.dim_x, self.N))
-		states[:, 0] = cur_state
+        if controls is None:
+            controls = np.zeros((self.dim_u, self.N))
+        states = np.zeros((self.dim_x, self.N))
+        states[:, 0] = cur_state
 
-		for i in range(1, self.N):
-			states[:,i], _ = self.dynamics.forward_step(states[:, i - 1], controls[:, i - 1])
+        for i in range(1, self.N):
+            states[:,i], _ = self.dynamics.forward_step(states[:, i - 1], controls[:, i - 1])
 
-		self.cost.init_cost(obs_list)
+        self.cost.init_cost(obs_list)
 
-		J = self.cost.get_cost(states, controls)
+        J = self.cost.get_cost(states, controls)
 
-		converged = False
+        converged = False
 
-		# have_not_updated = 0
-		for i in range(self.steps):
-			K_closed_loop, k_open_loop = self.backward_pass(states, controls)
-			updated = False
-			for alpha in self.alphas:
-				X_new, U_new, J_new = self.forward_pass(states, controls, K_closed_loop, k_open_loop, alpha)
-				if J_new <= J:
-					if np.abs((J - J_new) / J) < self.tol:
-						converged = True
-					states, controls, J = X_new, U_new, J_new
-					updated = True
-					break
-			if updated:
-				self.lambad *= 0.7
-			else:
-				status = 2
-				break
-			# self.lambad = min(max(self.lambad_min, self.lambad), self.lambad_max)
-			# self.lambad = max(self.lambad_min, self.lambad)
+        # have_not_updated = 0
+        for i in range(self.steps):
+            K_closed_loop, k_open_loop = self.backward_pass(states, controls)
+            updated = False
+            for alpha in self.alphas:
+                X_new, U_new, J_new = self.forward_pass(states, controls, K_closed_loop, k_open_loop, alpha)
+                if J_new <= J:
+                    if np.abs((J - J_new) / J) < self.tol:
+                        converged = True
+                    states, controls, J = X_new, U_new, J_new
+                    updated = True
+                    break
+            if updated:
+                self.lambad *= 0.7
+            else:
+                status = 2
+                break
+            # self.lambad = min(max(self.lambad_min, self.lambad), self.lambad_max)
+            # self.lambad = max(self.lambad_min, self.lambad)
 
-			if converged:
-				status = 1
-				break
-		t_process = time.time() - time0
-		# print("step, ", i, "alpha:", alpha)
-		print("iLQR solve: {:.6f}s, status: {}".format(t_process, status))
-		if record:
-			# get parameters for FRS
-			K_closed_loop, _ = self.backward_pass(states, controls)
-			A, B = self.dynamics.get_AB_matrix(states, controls)
-		else:
-			K_closed_loop = None
-			A, B = None, None
-		return states, controls, t_process, status, K_closed_loop, A, B
+            if converged:
+                status = 1
+                break
+        t_process = time.time() - time0
+        # print("step, ", i, "alpha:", alpha)
+        print("iLQR solve: {:.6f}s, status: {}".format(t_process, status))
+        if record:
+            # get parameters for FRS
+            K_closed_loop, _ = self.backward_pass(states, controls)
+            A, B = self.dynamics.get_AB_matrix(states, controls)
+        else:
+            K_closed_loop = None
+            A, B = None, None
+        return states, controls, t_process, status, K_closed_loop, A, B
--- src/Tasks/core_acc/scripts/trajectory.py
+++ src/Tasks/core_acc/scripts/trajectory.py
@@ -5,120 +5,120 @@ from scipy.spatial.transform import Rotation
 from MPC import State
 
 class Trajectory():
-	def __init__(self, max_list_size=200, fixed=-1):
-		self.lock = RLock()
-		self.max_list_size = max_list_size
-		self.fixed = fixed
+    def __init__(self, max_list_size=200, fixed=-1):
+        self.lock = RLock()
+        self.max_list_size = max_list_size
+        self.fixed = fixed
 
-		self.trajectory = []
+        self.trajectory = []
 
-		self.ref_d = 0.2 # spatial separation between reference trajectory points
-	
-	def _odom_to_state(self, odom_msg, prev_index=-1):
-		with self.lock:
-			# timestamp
-			t = odom_msg.header.stamp
-			# position
-			x = odom_msg.pose.pose.position.x
-			y = odom_msg.pose.pose.position.y
-			# orientation
-			rot_vec = Rotation.from_quat([
-				odom_msg.pose.pose.orientation.x, odom_msg.pose.pose.orientation.y,
-				odom_msg.pose.pose.orientation.z, odom_msg.pose.pose.orientation.w,
-			]).as_rotvec()
-			psi = rot_vec[2]
-			# linear velocity
-			if prev_index is not None and self.size() > 0:
-				dx = x - self.trajectory[prev_index].state[0]
-				dy = y - self.trajectory[prev_index].state[1]
-				dt = (t - self.trajectory[prev_index].t).to_sec()
-				v = np.sqrt(dx * dx + dy * dy) / dt
-			else:
-				v = 0
-			# return state
-			return State(np.array([x, y, v, psi]), t)
+        self.ref_d = 0.2 # spatial separation between reference trajectory points
+    
+    def _odom_to_state(self, odom_msg, prev_index=-1):
+        with self.lock:
+            # timestamp
+            t = odom_msg.header.stamp
+            # position
+            x = odom_msg.pose.pose.position.x
+            y = odom_msg.pose.pose.position.y
+            # orientation
+            rot_vec = Rotation.from_quat([
+                odom_msg.pose.pose.orientation.x, odom_msg.pose.pose.orientation.y,
+                odom_msg.pose.pose.orientation.z, odom_msg.pose.pose.orientation.w,
+            ]).as_rotvec()
+            psi = rot_vec[2]
+            # linear velocity
+            if prev_index is not None and self.size() > 0:
+                dx = x - self.trajectory[prev_index].state[0]
+                dy = y - self.trajectory[prev_index].state[1]
+                dt = (t - self.trajectory[prev_index].t).to_sec()
+                v = np.sqrt(dx * dx + dy * dy) / dt
+            else:
+                v = 0
+            # return state
+            return State(np.array([x, y, v, psi]), t)
 
-	def add_odom_state(self, odom_msg):
-		with self.lock:
-			state = self._odom_to_state(odom_msg)
-			# drop states when reaching max list size
-			while self.size() > self.max_list_size - 1:
-				self.trajectory.pop(self.fixed + 1)
-			# add new state to trajectory
-			self.trajectory.append(state)
-	
-	
-	def update_odom_state(self, odom_msg, index):
-		with self.lock:
-			# update state in trajectory
-			state = self._odom_to_state(odom_msg, prev_index=index - 1 if index > 0 else None)
-			while index >= self.size(): # if list too short pad with given state
-				self.trajectory.append(state)
-			self.trajectory[index] = state
+    def add_odom_state(self, odom_msg):
+        with self.lock:
+            state = self._odom_to_state(odom_msg)
+            # drop states when reaching max list size
+            while self.size() > self.max_list_size - 1:
+                self.trajectory.pop(self.fixed + 1)
+            # add new state to trajectory
+            self.trajectory.append(state)
+    
+    
+    def update_odom_state(self, odom_msg, index):
+        with self.lock:
+            # update state in trajectory
+            state = self._odom_to_state(odom_msg, prev_index=index - 1 if index > 0 else None)
+            while index >= self.size(): # if list too short pad with given state
+                self.trajectory.append(state)
+            self.trajectory[index] = state
 
-	def truncate_trajectory(self, x, y):
-		"""
-		Truncate trajectory while ignoring fixed data points
-		"""
-		with self.lock:
-			if self.size() <= self.fixed + 1:
-				return
-			
-			trajectory_np = self.get_trajectory_np()[:, self.fixed + 1:]
-			current_p = np.array([[x], [y]])
-			closest_p_index = np.argmin(np.linalg.norm(trajectory_np[0:2] - current_p, axis=0))
-			
-			# drop states on trajectory behind closest point
-			for _ in range(closest_p_index):
-				self.trajectory.pop(self.fixed + 1)
+    def truncate_trajectory(self, x, y):
+        """
+        Truncate trajectory while ignoring fixed data points
+        """
+        with self.lock:
+            if self.size() <= self.fixed + 1:
+                return
+            
+            trajectory_np = self.get_trajectory_np()[:, self.fixed + 1:]
+            current_p = np.array([[x], [y]])
+            closest_p_index = np.argmin(np.linalg.norm(trajectory_np[0:2] - current_p, axis=0))
+            
+            # drop states on trajectory behind closest point
+            for _ in range(closest_p_index):
+                self.trajectory.pop(self.fixed + 1)
 
-	def get_trajectory(self):
-		with self.lock:
-			return self.trajectory
+    def get_trajectory(self):
+        with self.lock:
+            return self.trajectory
 
-	def size(self):
-		with self.lock:
-			return len(self.trajectory)
-	
-	def get_trajectory_np(self):
-		return np.array([state.state for state in self.get_trajectory()]).T
+    def size(self):
+        with self.lock:
+            return len(self.trajectory)
+    
+    def get_trajectory_np(self):
+        return np.array([state.state for state in self.get_trajectory()]).T
 
-	def get_reference_trajectory(self, min_size=1, prefix=[]):
-		"""
-		Returns:
-			ref_trajectory: (4, N) array with (x, y, v, psi) states and N >= min_size
-		"""
-		trajectory = prefix + self.get_trajectory()
-		
-		# TODO if empty
+    def get_reference_trajectory(self, min_size=1, prefix=[]):
+        """
+        Returns:
+            ref_trajectory: (4, N) array with (x, y, v, psi) states and N >= min_size
+        """
+        trajectory = prefix + self.get_trajectory()
+        
+        # TODO if empty
 
-		if self.ref_d < 0.01: # avoiding infinite loop
-			return np.repeat([trajectory[0].state], min_size, axis=0).T
+        if self.ref_d < 0.01: # avoiding infinite loop
+            return np.repeat([trajectory[0].state], min_size, axis=0).T
 
-		# obtain trajectory with points uniformly spaced with distance self.ref_d
-		ref_trajectory = [trajectory[0].state]
-		distance = 0
-		last_state = trajectory[0]
-		for state in trajectory[1:]:
-			difference = state.state - last_state.state
-			distance += np.linalg.norm(difference[0:2])
-			
-			for i in range(int(distance / self.ref_d)):
-				alpha = (i+1) * self.ref_d / distance
-				ref_state = last_state.state + alpha * difference
-				ref_trajectory.append(ref_state)
-				distance = distance - self.ref_d
-			last_state = state
-		
-		# fill reference trajectory with latest 
-		while len(ref_trajectory) < min_size:
-			ref_trajectory.append(trajectory[-1].state)
+        # obtain trajectory with points uniformly spaced with distance self.ref_d
+        ref_trajectory = [trajectory[0].state]
+        distance = 0
+        last_state = trajectory[0]
+        for state in trajectory[1:]:
+            difference = state.state - last_state.state
+            distance += np.linalg.norm(difference[0:2])
+            
+            for i in range(int(distance / self.ref_d)):
+                alpha = (i+1) * self.ref_d / distance
+                ref_state = last_state.state + alpha * difference
+                ref_trajectory.append(ref_state)
+                distance = distance - self.ref_d
+            last_state = state
+        
+        # fill reference trajectory with latest 
+        while len(ref_trajectory) < min_size:
+            ref_trajectory.append(trajectory[-1].state)
 
-		return np.array(ref_trajectory).T
-	
-	def set_reference_velocity(self, v, dt):
-		self.ref_d = v * dt
+        return np.array(ref_trajectory).T
+    
+    def set_reference_velocity(self, v, dt):
+        self.ref_d = v * dt
 
-	def length(self):
-		trajectory_np = self.get_trajectory_np()
-		return np.sum(np.linalg.norm(trajectory_np[0:2, 1:] - trajectory_np[0:2, :-1], axis=0))
+    def length(self):
+        trajectory_np = self.get_trajectory_np()
+        return np.sum(np.linalg.norm(trajectory_np[0:2, 1:] - trajectory_np[0:2, :-1], axis=0))
